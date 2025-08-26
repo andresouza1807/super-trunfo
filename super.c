32b2bbae23ccfcb1e0ea750fd7f3c96299e55676
andre <asouza1807@gmail.com>
2025-08-26 19:26:30 -0300

    first commit

---
+++ super.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Carta
+{
+    char estado[3];
+    char codigo[4];
+    char nome[30];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+};
+ int main(){
+    struct Carta c1, c2;
+    int escolha;
+  
+    
+ }
